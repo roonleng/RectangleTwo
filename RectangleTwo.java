4b09a5bb7de69467a2693e20aa5cf17f765a3356
roonleng <42977073+roonleng@users.noreply.github.com>
2018-10-02 13:38:28 -0400

    Add files via upload

---
+++ RectangleTwo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class RectangleTwo 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter the width of the rectangle:);
+	}
+
+}
