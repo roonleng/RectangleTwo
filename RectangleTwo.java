0eef2059365c1cbaa54cc54e931fad7f208e2d29
roonleng <42977073+roonleng@users.noreply.github.com>
2018-10-02 23:33:10 -0400

    Add files via upload

--- RectangleTwo.java
+++ RectangleTwo.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 import java.util.Scanner;
 public class RectangleTwo 
 {
@@ -5,7 +8,21 @@ public class RectangleTwo
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter the width of the rectangle:);
+		System.out.print("Please enter the width of the rectangle: ");
+		double width = in.nextDouble();
+		in.nextLine();
+		System.out.print("Please enter the length of the rectangle: ");
+		double length = in.nextDouble();
+		in.nextLine();
+		in.close();
+		
+		double area = length*width;
+		double perimeter = (2*width) + (2*length);
+		double diagonal = Math.sqrt((length*length) +(width*width));
+		
+		System.out.printf("The area is: %6.2f", area);
+		System.out.printf("The area is: %6.2f", perimeter);
+		System.out.printf("The area is %6.2f",  diagonal);
 	}
 
 }
